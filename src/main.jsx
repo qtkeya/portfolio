c4a019a0ba586cb085259a37d9c252319e9ed2a5
keya sueno <144825182+qtkeya@users.noreply.github.com>
2025-03-02 05:50:58 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
-=======
 import '@fontsource/poppins'
->>>>>>> 0ac5c3d (I've created new components and route it using react-router-dom)
 import './index.css'
 import App from './App.jsx'
 
@@ -12,7 +9,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
-<<<<<<< HEAD
-=======
-
->>>>>>> 0ac5c3d (I've created new components and route it using react-router-dom)
